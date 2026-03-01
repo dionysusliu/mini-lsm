9fbb5eeb3f0180b823cf2440273d5ecfdc761336
chuang <chuangliu0815@gmail.com>
2026-03-01 11:09:40 -0800

    Merge Iterator
    Since we have multiple memtables in memory, and the read path would try
    to scan through them to find the latest value, it is reasonable to
    implement an iterator to automate this process underneath.
    
    Now that we've have already have iterator for each memtable, we can merge them by
    doing something similiar to "merge k sorted list":
    - put all iterators inside a heap, ordered by (key, table-version)
    - for each value, the heap top should be the iterator with latest
      version of that KV pair
    
    For each different value, we need to carefully clear the invalid
    iterators, namely:
    1. iterators not accessible (occurring errors like disk failure)
    2. iterators with invalid keys (out of scoped)
    3. iterators with stale keys (out of date)
    All these logics are handled by the `.next()` method
    
    The tricky part of this task is to "avoid copying any keys". The
    `MergeIterator` owns the iterators to each memtable, while each
    memtable owns skiplist iterators, while each skiplist iterator
    "borrows" the key / value. As a graph
    
    ```
    mergeIter --owns--> storageIter --owns--> skiplistIter --borrows--> key
    data
    ```
    This means to avoid copying the data, we can copy the iterators around
    but not the data themselves, and we need to make sure the "lifetime" of
    iterators in the 'upstream' of this graph would not outlive the lifetime
    of iterators / data in the 'downstream'. This lifetime rule should be
    enforced in code when implementing `impl StorageIterator` trait for the
    `MergeIterator`.

--- mini-lsm-starter/src/iterators/merge_iterator.rs
+++ mini-lsm-starter/src/iterators/merge_iterator.rs
@@ -17,10 +17,13 @@
 
 use std::cmp::{self};
 use std::collections::BinaryHeap;
+use std::collections::binary_heap::PeekMut;
+use std::fmt::Binary;
 
 use anyhow::Result;
 
 use crate::key::KeySlice;
+use crate::mem_table;
 
 use super::StorageIterator;
 
@@ -59,7 +62,26 @@ pub struct MergeIterator<I: StorageIterator> {
 
 impl<I: StorageIterator> MergeIterator<I> {
     pub fn create(iters: Vec<Box<I>>) -> Self {
-        unimplemented!()
+        if iters.is_empty() {
+            return MergeIterator {
+                iters: BinaryHeap::new(),
+                current: None,
+            };
+        }
+
+        let mut heap = BinaryHeap::new();
+        for (i, iter) in iters.into_iter().enumerate() {
+            if iter.is_valid() {
+                heap.push(HeapWrapper(i, iter));
+            }
+        }
+
+        let current = heap.pop();
+
+        MergeIterator {
+            iters: heap,
+            current,
+        }
     }
 }
 
@@ -68,19 +90,62 @@ impl<I: 'static + for<'a> StorageIterator<KeyType<'a> = KeySlice<'a>>> StorageIt
 {
     type KeyType<'a> = KeySlice<'a>;
 
-    fn key(&self) -> KeySlice {
-        unimplemented!()
+    fn key(&self) -> KeySlice<'_> {
+        self.current.as_ref().unwrap().1.key()
     }
 
     fn value(&self) -> &[u8] {
-        unimplemented!()
+        self.current.as_ref().unwrap().1.value()
     }
 
     fn is_valid(&self) -> bool {
-        unimplemented!()
+        self.current.is_some()
     }
 
     fn next(&mut self) -> Result<()> {
-        unimplemented!()
+        // while let Some(iter) = self.iters.peek_mut() {
+        //     if iter.1.key() == self.current.as_ref().unwrap().1.key() {
+        //         // stale keys
+        //         continue;
+        //     }
+        //     let poped_iter = PeekMut::pop(iter);
+        //     *self.current.as_mut().unwrap() = poped_iter;
+        //     return Ok(());
+        // }
+        //
+        // // no more iter if the control flow gets here
+        // self.current = None;
+        // Ok(())
+        //
+
+        let current = self.current.as_ref().unwrap();
+        let current_key = current.1.key();
+
+        // drain heap iterators sitting on the same stale key
+        while let Some(mut iter) = self.iters.peek_mut() {
+            if iter.1.key() == current_key {
+                if let Err(e) = iter.1.next() {
+                    PeekMut::pop(iter);
+                    return Err(e);
+                }
+                if !iter.1.is_valid() {
+                    PeekMut::pop(iter);
+                }
+            } else {
+                break;
+            }
+        }
+
+        // advance current itself
+        let mut current = self.current.take().unwrap();
+        current.1.next()?;
+        if current.1.is_valid() {
+            self.iters.push(current);
+        }
+
+        // promote new minimum to current
+        self.current = self.iters.pop();
+
+        Ok(())
     }
 }
